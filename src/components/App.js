f5659882fa610605a5b114b28f6980428d870824
Your Name <amitabhyo@gmail.com>
2021-04-08 12:27:06 +0530

    approach built

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
-import React, { useCallback } from 'react'
+import React, { useState } from 'react'
 import Node from './Node'
 
 import './App.css';
 
 
 const App = () => {
+    const [InputMode, setInputMode] = useState('SOURCE');
+    //SOURCE, DESTINATION, OBSTACLES
+    const [GridData,setGridData] = useState({
+        source:null,
+        destination:null,
+        obstacles:{}
+    })
     const generateGrid = () => {
         const Grid = [];
         for (let i = 0; i < 1500; i++)
-            Grid.push(<Node number={i} />);
+            Grid.push(<Node number={i} key={i} />);
         return Grid;
     }
     return (
