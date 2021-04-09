ed2f4c5c5e9f0c544b9a7057f79d517361b0c5a8
blade365z <amitabhyo@gmail.com>
2021-04-09 17:26:24 +0530

    grid click events added

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,64 @@ import Grid from './Grid';
 import './App.css';
 
 
+const initialzeObjectForGrid = (row, col) => {
+    //Grid Object Initalizer
+    /*
+        {
+            Node-Number:Type
+        }
+    */
+    let object = {};
+    let source = 458;
+    let destination = 477;
+    for (let i = 0; i < (row * col); i++) {
+        if (i === source)
+            object[i] = 'SOURCE';
+        else if (i === destination)
+            object[i] = 'DESTINATION'
+        else
+            object[i] = 'PATH'
+    }
+    return object;
+}
 const App = () => {
+    const [GridData, setGridData] = useState(
+        initialzeObjectForGrid(20, 50)
+    );
+    const setUpObstacles = useCallback((node, type) => {
+        setGridData(prevState => ({
+            ...prevState,
+            [node]: type
+        }))
+    })
+
+    const setDraggedNode = (source, target) => {
+        let typeTemp = GridData[source];
+        setGridData(prevState => ({
+            ...prevState,
+            [target]: typeTemp,
+            [source]: 'PATH',
+        }));
+    }
+
     return (
         <div>
-            <h2 style={{ textAlign: 'center', margin: '0px' }}>Path Visualizer</h2>
+            <div className="nav">
+                <div><h1 style={{ margin: '0' }}>Path Visualizer</h1></div>
+                <div style={{ marginLeft: 'auto', paddingTop: '10px' }}>
+                    <select className="drop-down">
+                        <option>Dijkstra's Algorithm</option>
+                        <option>A* Algorithm</option>
+                    </select>
+                    <button className="button">Search Path</button>
+                </div>
+            </div>
             <div className="grid-container">
-                <Grid />
+                <Grid
+                    GridData={GridData}
+                    setUpObstacles={setUpObstacles}
+                    setDraggedNode={setDraggedNode}
+                />
             </div>
         </div>
     )
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,56 +1,66 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 
-const initialzeObjectForGrid = (row, col) => {
-    //Grid Object Initalizer
-    /*
-        {
-            Node-Number:Type
-        }
-    */
-    let object = {};
-    let source = 458;
-    let destination = 477;
-    for (let i = 0; i < (row * col); i++) {
-        if (i === source)
-            object[i] = 'SOURCE';
-        else if (i === destination)
-            object[i] = 'DESTINATION'
-        else
-            object[i] = 'PATH'
-    }
-    return object;
-}
 
 
 //Grid Component
-const Grid = () => {
-    const [GridData, setGridData] = useState(
-        initialzeObjectForGrid(30, 50)
-    );
+const Grid = ({ GridData, setUpObstacles, setDraggedNode }) => {
+
     const [MouseHold, setMouseHold] = useState(false);
     const setObstacle = (node) => {
         if (MouseHold && GridData[node] !== 'SOURCE' && GridData[node] !== 'DESTINATION') {
-            let gridData = {};
-            Object.keys(GridData).map(element => {
-                if (element === node) {
-                    gridData[element] = 'OBSTACLE';
-
-                } else {
-                    gridData[element] = GridData[element];
-                }
-            })
-            setGridData(gridData);
+            (GridData[node] === 'OBSTACLE') && setUpObstacles(node, 'PATH');
+            (GridData[node] === 'PATH') && setUpObstacles(node, 'OBSTACLE');
         }
     }
+    const initiateObstacleSetup = (node) => {
+        setMouseHold(true)
+        if (GridData[node] !== 'SOURCE' && GridData[node] !== 'DESTINATION') {
+            (GridData[node] === 'OBSTACLE') && setUpObstacles(node, 'PATH');
+            (GridData[node] === 'PATH') && setUpObstacles(node, 'OBSTACLE');
+        }
+    }
+    const dragNode = (e, node) => {
+        //Make sure mouse hold is false
+        setMouseHold(false);
+        e.dataTransfer.setData('source', node);
+    }
+    const draggedOver = (e, node) => {
+        e.preventDefault();
+        e.dataTransfer.dropEffect = "move";
+    }
+    const droppedOnGrid = (e, node) => {
+        let source = e.dataTransfer.getData('source');
+        if (GridData[node] !== 'SOURCE' && GridData[node] !== 'DESTINATION' && GridData[node] !== 'OBSTACLE') {
+            setDraggedNode(source, node);
 
+        }
+    }
     return (
-        <div className="grid" onMouseDown={() => setMouseHold(true)} onMouseUp={() => setMouseHold(false)} >
+        <div className="grid"  >
             {
                 Object.keys(GridData).map((node, i) => {
-                    return <div className={"node " + GridData[node].toLowerCase()} key={i} title={"node: " + node} onMouseOver={() => setObstacle(node)}>
-                        {GridData[node] === 'SOURCE' && <i className="fas fa-male" draggable></i>}
-                        {GridData[node] === 'DESTINATION' && <i className="fas fa-map-marker-alt "></i>}
+                    return <div className={"node " + GridData[node].toLowerCase()} key={i} title={"node: " + node} onMouseEnter={() => setObstacle(node)} onMouseDown={() => initiateObstacleSetup(node)} onMouseUp={() => setMouseHold(false)}    >
+                        {
+                            GridData[node] === 'SOURCE' && <div
+                                draggable onDragStart={(e) => dragNode(e, node)}>
+                                <i className="fas fa-male" ></i>
+                            </div>
+                        }
+
+                        {
+                            GridData[node] === 'DESTINATION' && <div
+                                draggable onDragStart={(e) => dragNode(e, node)}>
+                                <i className="fas fa-map-marker-alt "></i>
+                            </div>
+                        }
+                        {
+                            GridData[node] === 'PATH' && <div
+                                onDragOver={(e) => draggedOver(e, node)}
+                                onDrop={(e) => droppedOnGrid(e, node)}
+                                style={{ width: '100%', height: '100%' }} >
+                            </div>
+                        }
                     </div>
                 })
             }
